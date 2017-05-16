58bbfc94807d1bda7e2f65d9a43115ee41fd21dc
Joana Trindade <jmftrindade@gmail.com>
2017-05-16 17:26:32 -0400

    do not save histograms: only save raw counters for op durations and sizes

--- examples/random/main.go
+++ examples/random/main.go
@@ -34,37 +34,57 @@ var (
 	normDomain        = flag.Float64("normal.domain", 0.0002, "The domain for the normal distribution.")
 	normMean          = flag.Float64("normal.mean", 0.00001, "The mean for the normal distribution.")
 	oscillationPeriod = flag.Duration("oscillation-period", 10*time.Minute, "The duration of the rate oscillation period.")
+	sizesNormDomain   = flag.Float64("normal.sdomain", 10240, "The domain for the normal distribution.")
+	sizesNormMean     = flag.Float64("normal.smean", 1024, "The mean for the normal distribution.")
 )
 
 var (
-/*
-	// Create a summary to track fictional interservice RPC latencies for three
-	// distinct services with different latency distributions. These services are
-	// differentiated via a "service" label.
-	rpcDurations = prometheus.NewSummaryVec(
+	/*
+		// Create a summary to track fictional interservice RPC latencies for three
+		// distinct services with different latency distributions. These services are
+		// differentiated via a "service" label.
+		rpcDurations = prometheus.NewSummaryVec(
+			prometheus.SummaryOpts{
+				Name:       "rpc_durations_seconds",
+				Help:       "RPC latency distributions.",
+				Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+			},
+			[]string{"service"},
+		)
+		// The same as above, but now as a histogram, and only for the normal
+		// distribution. The buckets are targeted to the parameters of the
+		// normal distribution, with 20 buckets centered on the mean, each
+		// half-sigma wide.
+		rpcDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
+			Name:    "rpc_durations_histogram_seconds",
+			Help:    "RPC latency distributions.",
+			Buckets: prometheus.LinearBuckets(*normMean-5**normDomain, .5**normDomain, 20),
+		})
+	*/
+
+	lineageOpDurations = prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
-			Name:       "rpc_durations_seconds",
-			Help:       "RPC latency distributions.",
-			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+			Name: "lineage_op_durations_seconds",
+			Help: "Data lineage op durations distribution (in seconds).",
 		},
-		[]string{"service"},
+		[]string{"user", "op", "dataset"},
 	)
-	// The same as above, but now as a histogram, and only for the normal
-	// distribution. The buckets are targeted to the parameters of the
-	// normal distribution, with 20 buckets centered on the mean, each
-	// half-sigma wide.
-	rpcDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:    "rpc_durations_histogram_seconds",
-		Help:    "RPC latency distributions.",
-		Buckets: prometheus.LinearBuckets(*normMean-5**normDomain, .5**normDomain, 20),
-	})
-*/
+
+	lineageOpSizes = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "lineage_op_sizes_bytes",
+			Help: "Data lineage op sizes distribution (in bytes).",
+		},
+		[]string{"user", "op", "dataset"},
+	)
+
+/*
 	// Data lineage events sizes in data bytes.
 	lineageOpSizesHistogram = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "lineage_op_sizes_histogram_bytes",
 			Help:    "Data lineage op sizes distributions (in bytes).",
-			Buckets: prometheus.LinearBuckets(*normMean-5**normDomain, .5**normDomain, 20),
+			Buckets: prometheus.LinearBuckets(*sizesNormMean-5**sizesNormDomain, .5**sizesNormDomain, 20),
 		},
 		[]string{"user", "op", "dataset"},
 	)
@@ -78,17 +98,13 @@ var (
 		},
 		[]string{"user", "op", "dataset"},
 	)
-
+*/
 )
 
 func init() {
-	// Register the summary and the histogram with Prometheus's default registry.
-	//	prometheus.MustRegister(rpcDurations)
-	//	prometheus.MustRegister(rpcDurationsHistogram)
-
 	// Register the data lineage metrics with Prometheus's default registry.
-	prometheus.MustRegister(lineageOpSizesHistogram)
-        prometheus.MustRegister(lineageOpDurationsHistogram)
+	prometheus.MustRegister(lineageOpSizes)
+	prometheus.MustRegister(lineageOpDurations)
 }
 
 func main() {
@@ -99,52 +115,73 @@ func main() {
 	oscillationFactor := func() float64 {
 		return 2 + math.Sin(math.Sin(2*math.Pi*float64(time.Since(start))/float64(*oscillationPeriod)))
 	}
-/*
-	// Periodically record some sample latencies for the three services.
-	go func() {
-		for {
-			v := rand.Float64() * *uniformDomain
-			rpcDurations.WithLabelValues("uniform").Observe(v)
-			time.Sleep(time.Duration(100*oscillationFactor()) * time.Millisecond)
-		}
-	}()
+	/*
+		// Periodically record some sample latencies for the three services.
+		go func() {
+			for {
+				v := rand.Float64() * *uniformDomain
+				rpcDurations.WithLabelValues("uniform").Observe(v)
+				time.Sleep(time.Duration(100*oscillationFactor()) * time.Millisecond)
+			}
+		}()
 
-	go func() {
-		for {
-			v := (rand.NormFloat64() * *normDomain) + *normMean
-			rpcDurations.WithLabelValues("normal").Observe(v)
-			rpcDurationsHistogram.Observe(v)
-			time.Sleep(time.Duration(75*oscillationFactor()) * time.Millisecond)
-		}
-	}()
+		go func() {
+			for {
+				v := (rand.NormFloat64() * *normDomain) + *normMean
+				rpcDurations.WithLabelValues("normal").Observe(v)
+				rpcDurationsHistogram.Observe(v)
+				time.Sleep(time.Duration(75*oscillationFactor()) * time.Millisecond)
+			}
+		}()
 
-	go func() {
-		for {
-			v := rand.ExpFloat64() / 1e6
-			rpcDurations.WithLabelValues("exponential").Observe(v)
-			time.Sleep(time.Duration(50*oscillationFactor()) * time.Millisecond)
-		}
-	}()
-*/
+		go func() {
+			for {
+				v := rand.ExpFloat64() / 1e6
+				rpcDurations.WithLabelValues("exponential").Observe(v)
+				time.Sleep(time.Duration(50*oscillationFactor()) * time.Millisecond)
+			}
+		}()
+	*/
 	// Periodically record randomly generated sample data lineage events.
 	go func() {
 		for {
 			users := []string{"alice", "bob"}
 			datasets := []string{"file_a", "file_b", "file_c"}
 			opTypes := []string{"read", "write"}
+			opSizes := []float64{512.0, 1024.0, 2048.0}
+			opDurations := []float64{1.0, 2.0, 3.0}
+			/*
+				v := (rand.NormFloat64() * *sizesNormDomain) + *sizesNormMean
+				lineageOpSizesHistogram.WithLabelValues(
+					users[rand.Intn(len(users))],
+					opTypes[rand.Intn(len(opTypes))],
+					datasets[rand.Intn(len(datasets))]).Observe(v)
 
-			v := (rand.NormFloat64() * *normDomain) + *normMean
-			lineageOpSizesHistogram.WithLabelValues(users[rand.Intn(len(users))],
-				opTypes[rand.Intn(len(opTypes))],
-				datasets[rand.Intn(len(datasets))]).Observe(v)
+				// Use a different random value, but still generate both of these in side-step.
+				v = (rand.NormFloat64() * *normDomain) + *normMean
+				lineageOpDurationsHistogram.WithLabelValues(users[rand.Intn(len(users))],
+					opTypes[rand.Intn(len(opTypes))],
+					datasets[rand.Intn(len(datasets))]).Observe(v)
+			*/
+			// Use a different random value, but still generate both of these in side-step.
+			opIdx := rand.Intn(len(opSizes))
+			userIdx := rand.Intn(len(users))
+			typeIdx := rand.Intn(len(opTypes))
+			datasetIdx := rand.Intn(len(datasets))
 
-                        // Use a different random value, but still generate both of these in side-step.
-			v = (rand.NormFloat64() * *normDomain) + *normMean
-			lineageOpDurationsHistogram.WithLabelValues(users[rand.Intn(len(users))],
-				opTypes[rand.Intn(len(opTypes))],
-				datasets[rand.Intn(len(datasets))]).Observe(v)
+			v := opSizes[opIdx]
+			lineageOpSizes.WithLabelValues(
+				users[userIdx],
+				opTypes[typeIdx],
+				datasets[datasetIdx]).Observe(v)
 
-                        // Sleep a little to add variance.
+			v = opDurations[opIdx]
+			lineageOpDurations.WithLabelValues(
+				users[userIdx],
+				opTypes[typeIdx],
+				datasets[datasetIdx]).Observe(v)
+
+			// Sleep a little to add variance.
 			time.Sleep(time.Duration(75*oscillationFactor()) * time.Millisecond)
 		}
 	}()
